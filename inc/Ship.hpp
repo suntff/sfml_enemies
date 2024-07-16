7d5cc916b556f2cd38674405e121fb44fda59c59
suntff <morakindaniil@gmail.com>
2024-07-16 19:05:50 +0200

    Update Ship.hpp

--- inc/Ship.hpp
+++ inc/Ship.hpp
@@ -3,116 +3,94 @@
 #include <iostream>
 #include <bullet.hpp>
 
-namespace mt
+namespace msp
 {
 
 	class Ship
 	{
-		const int m_bulletR = 10;
-		float m_v_bullet = 200;
+		const int bulletR = 10;
+		float v_bullet = 200;
 
-		float m_x, m_y;
-		float m_alpha;
-		float m_v;
-		float m_r;
-		sf::Texture m_textureShip;
-		sf::Sprite m_spriteShip;
-		float m_scaleFactor = 0.2f;
+		float x, y;
+		float alpha;
+		float v;
+		float r;
+		sf::Texture textureShip;
+		sf::Sprite spriteShip;
+		float scaleFactor = 0.2f;
 
 		Bullet bullet;
 
 	public:
 		Ship() = default;
-		// !!!
-		Ship(float x, float y)
-		{
-			m_x = x;
-			m_y = y;
-		}
 
 		bool Setup(float x, float y)
 		{
-			m_x = x;
-			m_y = y;
-			m_alpha = 0;
-			m_v = 0;
-
-			// Çàãðóçêà òåêñòóðû ôîíà
-			if (!m_textureShip.loadFromFile("assets\\falcon.png"))
+			this->x = x;
+			this->y = y;
+			alpha = 0;
+			v = 0;
+			if (!textureShip.loadFromFile("assets\\ship.png"))
 			{
-				std::cout << "Error while loading falcon.png" << std::endl;
+				std::cerr << "Error while loading ship.png" << std::endl;
 				return false;
 			}
-			m_spriteShip.setTexture(m_textureShip);
+			spriteShip.setTexture(textureShip);
 
-			m_spriteShip.setScale(m_scaleFactor, m_scaleFactor);
-			m_spriteShip.setOrigin(m_textureShip.getSize().x / 2, m_textureShip.getSize().y / 2);
-			m_spriteShip.setPosition(m_x, m_y);
-			m_spriteShip.setRotation(m_alpha);
+			spriteShip.setScale(scaleFactor, scaleFactor);
+			spriteShip.setOrigin(textureShip.getSize().x / 2, textureShip.getSize().y / 2);
+			spriteShip.setPosition(x, y);
+			spriteShip.setRotation(alpha);
 
-			if (m_textureShip.getSize().x > m_textureShip.getSize().y)
-				m_r = m_scaleFactor * m_textureShip.getSize().x / 2;
+			if (textureShip.getSize().x > textureShip.getSize().y)
+				r = scaleFactor * textureShip.getSize().x / 2;
 			else
-				m_r = m_scaleFactor * m_textureShip.getSize().y / 2;
+				r = scaleFactor * textureShip.getSize().y / 2;
 
 			//bullet.Setup(100, 100, 20, 10, 10);
 		}
 
-		void Attack()
+
+
+
+		void setVelocity(float dv,float dt)
 		{
-			float alphaRad = acos(-1) * m_alpha / 180;
-			bullet.Setup(m_x, m_y, m_bulletR, (m_v + m_v_bullet) * cos(alphaRad), (m_v + m_v_bullet) * sin(alphaRad));
-			bullet.Available(true);
-		}
-
-
-
-		void setVelocity(float dv)
-		{
-			m_v += dv;
+			v += dv*dt;
 			//std::cerr << m_v << std::endl;
 		}
 
-		float V() { return m_v; }
+		float V() { return v; }
 
 		void Move(float dt)
 		{
 			bullet.Move(dt);
-			float alphaRad = acos(-1) * m_alpha / 180;
-			m_x += m_v * cos(alphaRad) * dt;
-			m_y += m_v * sin(alphaRad) * dt;
-			m_spriteShip.setPosition(m_x, m_y);
+			float alphaRad = acos(-1) * alpha / 180;
+			x += v * cos(alphaRad) * dt;
+			y += v * sin(alphaRad) * dt;
+			spriteShip.setPosition(x, y);
 		}
 
-		float R() { return m_r; }
-		float X() { return m_x; }
-		float Y() { return m_y; }
+		float R() { return r; }
+		float X() { return x; }
+		float Y() { return y; }
 
-		void Rotate(float dalpha)
+		void Rotate(float dalpha,float dt)
 		{
-			m_alpha += dalpha;
-			m_spriteShip.setRotation(m_alpha);
+			alpha += dalpha*dt;
+			spriteShip.setRotation(alpha);
 		}
 
 		sf::Sprite Get()
 		{
-			return m_spriteShip;
+			return spriteShip;
 		}
 
-		bool BulletAvailable()
-		{
-			return bullet.Available();
-		}
-
-		void BulletReset()
-		{
-			bullet.Available(false);
-		}
 
 		sf::CircleShape GetBullet()
 		{
 			return bullet.Get();
 		}
+		float Alpha() { return alpha; };
 	};
 
 }
